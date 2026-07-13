db4485d28b24b428d3b54710da9e928f8fab2060
phanithmin-source <phanith.min@allweb.com.kh>
2026-07-13 09:50:17 +0700

    chore:src/apollo/client.ts

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,15 +1,17 @@
 import {
   ApolloClient,
-  HttpLink,
 } from "@apollo/client";
+import { BatchHttpLink } from "@apollo/client/link/batch-http";
 
 import { authLink } from "./authLink";
 import { tokenRefreshLink } from "./tokenRefreshLink";
 import { cache } from "./cache";
 
-const httpLink = new HttpLink({
+const httpLink = new BatchHttpLink({
   uri: import.meta.env.VITE_GRAPHQL_URL,
   credentials: "include",
+  batchMax: 10,
+  batchInterval: 10,
 });
 
 export const client = new ApolloClient({
